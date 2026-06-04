740b9dbbe20758f6bb9b14e6ea0ca9d1c9173d7f
zajacn <zajac@evolbio.mpg.de>
2026-06-04 19:02:53 +0000

    Deploying to gh-pages from @ zajacn/zajacn.github.io@43e5b92f14e00902acc6aaf4f82ec56fb4f49ea6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zajacn/nataliazajac.github.io",
+    "data-repo": "zajacn/zajacn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
